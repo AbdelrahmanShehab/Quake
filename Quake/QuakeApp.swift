d5a3d6e4c580476ef7ee3af8501e114cc396d7cc
abdoshehab7 <abdoshehab7@gmail.com>
2024-01-11 11:26:42 +0200

    Initial Commit

---
+++ Quake/QuakeApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuakeApp.swift
+//  Quake
+//
+//  Created by Abdelrahman Shehab on 11/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct QuakeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
